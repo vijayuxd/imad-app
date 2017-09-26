9d8d1eb83a7b04359e33f3233f780e59e49f69a9
vijay vasudevan <vijayvasudevan.uxd@gmail.com>
2017-09-26 08:12:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,6 +68,10 @@ app.get('/:articleName', function(req, res) {
   //function(variable[object])
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
